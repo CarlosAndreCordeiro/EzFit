ace604a0619e27f40b8cc1d26415ee16462e7e76
Carlos André <carloscordeiroconsultor@gmail.com>
2018-11-30 17:02:35 -0300

    Primeiro Commit
    Configuraçoes de banco e persistencia ok

---
+++ src/main/java/br/edu/ifpe/ezfit/Aluno.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifpe.ezfit;
+
+/**
+ *
+ * @author $Carlos Cordeiro <carloscordeiroconsultor@gmail.com>
+ */
+public class Aluno {
+
+    
+    
+    
+    
+    
+}
